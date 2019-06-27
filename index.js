0c48751d3de9a2cd53a0677e5a80452cc91fda90
Arthur Vinicius Soares Pereira <arthurs@take.net>
2019-06-27 15:27:50 -0300

    Added root endpoint

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const requests = new Requests();
 const app = express();
 app.use(express.json());
 
+app.post('/', (req, res) => {
+  requests.handleMessage(req, res);
+});
+
 app.post('/message/handler', (req, res) => {
   requests.handleMessage(req, res);
 });
