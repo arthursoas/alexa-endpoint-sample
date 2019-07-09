f2223613cc558d98f4d01c20575b94cf1cdc5e04
Arthur Vinicius Soares Pereira <arthurs@take.net>
2019-07-09 09:48:18 -0300

    Handling de addressIntent

--- src/api/intentRequests/controller.js
+++ src/api/intentRequests/controller.js
@@ -9,11 +9,44 @@ export const handleIntentRequest = (body, res) => {
     case 'SeeCollectionIntent':
       response = SeeCollectionIntentResponse(body);
       break;
+    case 'AddressIntent':
+      response = AddressIntentResponse(body);
+      break;
     default:
       res.send(400);
   }
 
   res.send(response);
+}
+
+const AddressIntentResponse = (body) => {
+  const { request } = body;
+  if (request.dialogState == 'STARTED' || request.dialogState == 'IN_PROGRESS') {
+    if (request.intent.slots.CollectionType.value === undefined) {
+      return elicitSlot('State');   
+    }
+  }
+
+  const outputSpeechText = generateAnswer(request);
+
+  const outputSpeech = {
+    type: opType.plainText,
+    text: outputSpeechText,
+    playBehavior: playBehavior.enqueue
+  };
+
+  const card = {
+    type: cType.standard,
+    title: 'Venha nos conhecer!',
+    text: outputSpeechText,
+    image: {
+      smallImageUrl: 'https://i2.wp.com/cantinhodena.com.br/wp-content/uploads/2015/11/compras-nos-estados-unidos-roupas-femininas-capa.jpg?resize=709%2C355&ssl=1',
+      largeImageUrl: 'https://s2.glbimg.com/wu_oq4FjGgamZZ61MpCBPyI8Vn8=/940x523/e.glbimg.com/og/ed/f/original/2018/07/03/assorted-blurred-background-boutique-994523.jpg'
+    }
+  };
+
+  const responseFormatter = new ResponseFormatter(outputSpeech, card, undefined, directives, false);
+  return responseFormatter.formatResponse();
 }
 
 const SeeCollectionIntentResponse = (body) => {
@@ -50,8 +83,16 @@ const SeeCollectionIntentResponse = (body) => {
     {
       type: 'Dialog.Delegate',
       updatedIntent: {
-        name: 'SeeCollectionIntent',
-        confirmationStatus: 'NONE'
+        name: 'AddressIntent',
+        confirmationStatus: 'NONE',
+        slots: {
+          RequiredeSlot: {
+            name: "RequiredeSlot",
+            value: "filled",
+            resolutions: {},
+            confirmationStatus: "NONE"
+          }
+        }
       }
     }
   ]
@@ -60,12 +101,25 @@ const SeeCollectionIntentResponse = (body) => {
   return responseFormatter.formatResponse();
 }
 
+
 // Private
 const generateAnswer = (request) => {
-  const answers = [
-    'Você está procurando por ' + request.intent.slots.ClothesType.value +
-    ' da coleção ' + request.intent.slots.CollectionType.value
-  ];
+  let answers = [];
+  switch (request.intent.name) {
+    case 'SeeCollectionIntent':
+      answers = [
+        'Vi que você está procurando por ' + request.intent.slots.ClothesType.value +
+        ' da coleção ' + request.intent.slots.CollectionType.value + '.' +
+        ' Visite nossa loja mais próxima para saber mais. Use o Cupom ALEXA10 e ganhe 10%' +
+        ' de desconto na sua compra!' 
+      ];
+      break;
+    case 'AddressIntent':
+      answers = [
+        'Nosso endereço em ' + request.intent.slots.State.value +
+        ' é Avenida das Laranjeiras, 1450, bairro Centro.'
+      ]
+  }
 
   return answers[Math.floor(Math.random()*answers.length)];
 }
@@ -85,6 +139,11 @@ const generateSlotElicitationAnswer = (slot) => {
         'Qual peça de roupa você tem interesse?'
       ];
       break;
+    case 'State':
+      answers = [
+        'Qual o estado você está?',
+        'Em qual estado você mora?'
+      ]
     default:
       answers = [
         'Não entendi.'
@@ -95,7 +154,6 @@ const generateSlotElicitationAnswer = (slot) => {
 }
 
 const elicitSlot = (slot) => {
-  console.log(slot);
   const outputSpeechText = generateSlotElicitationAnswer(slot);
 
   const outputSpeech = {
