9a7b9619183dae816bde79e2375ada4561bb133a
Arthur Vinicius Soares Pereira <arthurs@take.net>
2019-07-11 09:08:41 -0300

    Added console logs on requests types

--- src/api/handler/controller.js
+++ src/api/handler/controller.js
@@ -7,17 +7,21 @@ export const handleMessage = (req, res) => {
   const { body } = req;
   switch (body.request.type) {
     case 'LaunchRequest':
+      console.log('LaunchRequest');
       handleLaunchRequest(body, res);
       break;
     case 'IntentRequest':
+      console.log('IntentRequest ', body.request.intent.name)
       handleIntentRequest(body, res);
       break;
     case 'SessionEndedRequest':
+      console.log('SessionEndedRequest');
       handleSessionEndedRequest(body, res);
       break;
     case 'Unhandled':
-        handleUnhandledRequest(body, res);
-        break;
+      console.log('Unhandled');
+      handleUnhandledRequest(body, res);
+      break;
     default:
       res.status(400).send();
   }
